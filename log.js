0b405647d4867315bc544cbe704ba4458c5072ad
Jonathan Sherrell <dev@jomsh.cc>
2022-06-29 22:34:45 -0500

    logging

--- log.js
+++ log.js
@@ -1,4 +1,13 @@
 if (location.protocol != 'file:' && location.hostname != 'localhost' && location.search != '?test') {
-    fetch('https://j-kqz.s3.amazonaws.com/c?' + location.host + location.pathname, { mode: 'no-cors' }).catch(e => e)
+
+    const url = new URL('https://jomshcc.s3.us-east-2.amazonaws.com/c.gif')
+    if (document.referrer) { url.searchParams.set('r', document.referrer) }
+
+    fetch(url, {
+        mode: 'no-cors'   // prevent preflight shenanigans
+      , referrerPolicy: 'unsafe-url' // track precise URL
+      , cache: 'no-store' // never use cache
+    }).catch(e => e)
+
 }
 
