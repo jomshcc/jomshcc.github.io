95df0ac35f96af2ea7e9ac14dccd380d61d6295e
Jonathan Sherrell <dev@jomsh.cc>
2022-06-30 20:57:56 -0500

    update README, comments

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-# Web root
+# [jomsh.cc][1]—web root
+
+## Link page, all the rage
+
+These are everywhere now, and they make a lot of sense. They’re like a return to
+the 90’s homepage, but no blinking marquees.
+
+Makes for a nice, bite-sized project to practice web development, but with
+enough visibility to follow all the way through with fit and finish.
+
+## GitHub Pages
 
   - [jomsh.cc][1] — `ALIAS` record results in generated `A` records for
     [jomshcc.github.io][3] (`CNAME` flattening)
@@ -6,7 +16,7 @@
     automatically redirects to [jomsh.cc][1])
 
 Any Pages-enabled repositories without their own custom domain config (`CNAME`
-file) will appear as subdirectories ([jomsh.cc/REPO][1])
+file) will appear as subdirectories (`jomsh.cc/REPO/`).
 
 [1]: https://jomsh.cc
 [2]: https://www.jomsh.cc
--- log.js
+++ log.js
@@ -1,13 +1,15 @@
-if (location.protocol != 'file:' && location.hostname != 'localhost' && location.search != '?test') {
-
+// Don't log under testing/development conditions
+if (location.protocol != 'file:'     &&
+    location.hostname != 'localhost' &&
+    location.search   != '?test')
+{
     const url = new URL('https://jomshcc.s3.us-east-2.amazonaws.com/c.gif')
     if (document.referrer) { url.searchParams.set('r', document.referrer) }
 
-    fetch(url, {
-        mode: 'no-cors'   // prevent preflight shenanigans
-      , referrerPolicy: 'unsafe-url' // track precise URL
-      , cache: 'no-store' // never use cache
+    fetch(url, {                     // Opaque response is fine; prevent
+        mode: 'no-cors'              //  console errors related to CORS
+      , referrerPolicy: 'unsafe-url' // Track precise URL
+      , cache: 'no-store'            // Never use cache
     }).catch(e => e)
-
 }
 
